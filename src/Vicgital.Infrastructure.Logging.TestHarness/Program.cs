5c3251d8609b355a45f6f15ff7b9b3f617cb089f
Vicgital Admin <Vicgital@hotmail.com>
2026-04-18 12:42:23 -0600

    Add Service Collection

--- src/Vicgital.Infrastructure.Logging.TestHarness/Program.cs
+++ src/Vicgital.Infrastructure.Logging.TestHarness/Program.cs
@@ -1,38 +1,47 @@
 ﻿
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using Vicgital.Infrastructure.Logging.Configuration.Extensions;
+using Vicgital.Infrastructure.Logging.Extensions;
 
-Serilog.LoggerConfiguration config = Vicgital.Infrastructure.Logging.Configuration.LoggerConfigurationBuilder.BuildDefault( Serilog.Events.LogEventLevel.Information);
+Serilog.LoggerConfiguration config = Vicgital.Infrastructure.Logging.Configuration.LoggerConfigurationBuilder.BuildDefault(Serilog.Events.LogEventLevel.Information);
 
 config.EnrichWithRedacter(["password"]);
 
 config.WriteToFile($"C:\\temp\\log_{DateTime.UtcNow.ToFileTimeUtc()}.txt", Serilog.RollingInterval.Day);
 
-Serilog.ILogger logger = config.CreateLogger();
+IServiceCollection services = new ServiceCollection();
 
-logger.Information("Hello World");
+services.AddSerilogLogging(config.CreateLogger());
 
-logger.Information("User logged in with username: {Username} and password: {Password}", "testuser", "secretpassword");
+var provider = services.BuildServiceProvider();
+var _logger = provider.GetRequiredService<ILogger<Program>>();
+
+
+_logger.LogInformation("Hello World");
+
+_logger.LogInformation("User logged in with username: {Username} and password: {Password}", "testuser", "secretpassword");
 
 try
 {
 
-	// log random information logs 
-	for (int i = 0; i < 10; i++)
-	{
-		logger.Information("Random log message {Index}", i);    
-	}
+    // log random information logs 
+    for (int i = 0; i < 10; i++)
+    {
+        _logger.LogInformation("Random log message {Index}", i);
+    }
 
-	// log random warning logs 
-	for (int i = 0; i < 5; i++)
-	{ 
-		logger.Warning("Random warning message {Index}", i);
+    // log random warning logs 
+    for (int i = 0; i < 5; i++)
+    {
+        _logger.LogWarning("Random warning message {Index}", i);
     }
 
     throw new Exception("Test Exception");
 }
 catch (Exception ex)
 {
-	logger.Error(ex, "Test Error: message: {Message}", ex.Message);
+    _logger.LogError(ex, "Test Error: message: {Message}", ex.Message);
 }
 
 
---
+++ src/Vicgital.Infrastructure.Logging/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+
+namespace Vicgital.Infrastructure.Logging.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void AddSerilogLogging(this IServiceCollection services, ILogger logger)
+        {
+            services.AddLogging(configure =>
+            {
+                configure.AddSerilog(logger);
+            });
+        }
+
+    }
+}
